ef36236672b15108a1d4afc750ac143fdc1b85ab
Szymon Auguścik <szyauguscik@gmail.com>
2021-01-04 16:12:46 +0100

    First

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Design_Patterns_project
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("First commit");
         }
     }
 }
