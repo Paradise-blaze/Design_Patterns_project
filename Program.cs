ae4de4ed5e7633aa4b57caa56c895d31a8385ee8
Szymon Auguścik <szyauguscik@gmail.com>
2021-01-07 16:03:14 +0100

    TableInheritance class and simple tests

--- Program.cs
+++ Program.cs
@@ -10,4 +10,40 @@ namespace Design_Patterns_project
             Console.WriteLine("Mein Herz in Flamen");
         }
     }
+
+    class MythicalCreature
+    {
+        int health;
+        string name;
+
+        public MythicalCreature(int h, string n)
+        {
+            this.health = h;
+            this.name = n;
+        }
+    }
+
+    class Dragon : MythicalCreature
+    {
+        int fireCapacity;
+        int endurance;
+
+        public Dragon(int h, string n, int fc, int e) : base(h, n)
+        {
+            this.fireCapacity = fc;
+            this.endurance = e;
+        }
+    }
+
+    class RareDragon : Dragon
+    {
+        string specialSkill;
+        double preciousness;
+
+        public RareDragon(int h, string n, int fc, int e, string ss, double p) : base(h, n, fc, e)
+        {
+            this.specialSkill = ss;
+            this.preciousness = p;
+        }
+    }
 }
---
+++ TableInheritance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design_Patterns_project
+{
+    class TableInheritance
+    {
+        public void InheritSingle(Type className)
+        {
+
+        }
+
+        public void InheritClass(Type className)
+        {
+
+        }
+
+        public void InheritConcrete(Type className)
+        {
+
+        }
+    }
+}
