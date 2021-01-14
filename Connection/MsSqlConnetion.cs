5b3e794a5f81bc54fdba2d633543f277fedfdcd6
Maciej Koch <maciej.k00h@gmail.com>
2021-01-14 22:52:27 +0100

    add exceptions hadnling for exectuing sql queries

--- Connection/MsSqlConnetion.cs
+++ Connection/MsSqlConnetion.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -47,40 +48,6 @@ namespace Design_Patterns_project.Connection
             this._connection.Dispose();
             
         }
-
-
-        // SELECT
-        public string ExecuteSelectQuery(string sqlQuery){
-
-            SqlCommand command = new SqlCommand(sqlQuery,this._connection);
-            SqlDataReader dataReader = command.ExecuteReader();
-
-            string output = "";
-            int colsAmount = dataReader.FieldCount;
-            int colWidht = 12;
-   
-            // add header
-            for(int i=0; i<colsAmount; i++){
-                string colName = dataReader.GetName(i);
-                output += colName;
-                for(int j=0; j<(colWidht - colName.Length); j++){
-                    output += " ";
-                }
-                output += "|";  
-            }
-
-            output += "\n";
-            for(int j=0; j<(colsAmount*colWidht)+colsAmount; j++){output += "-";}
-            output += "\n";
-
-            // add records
-            while(dataReader.Read()){
-                output += ReadSingleRow((IDataRecord)dataReader,colsAmount,colWidht);
-            }
-
-            return output;
-        }
-
         private string ReadSingleRow(IDataRecord record, int colsAmount, int colWidht)
         {   
             string recordString = "";
@@ -96,12 +63,85 @@ namespace Design_Patterns_project.Connection
             return recordString+"\n";
         }
 
+        // SELECT
+        public string ExecuteSelectQuery(string sqlQuery){
+
+            SqlCommand command = new SqlCommand(sqlQuery,this._connection);
+
+            try{
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                string output = "";
+                int colsAmount = dataReader.FieldCount;
+                int colWidht = 12;
+    
+                // add header
+                for(int i=0; i<colsAmount; i++){
+                    string colName = dataReader.GetName(i);
+                    output += colName;
+                    for(int j=0; j<(colWidht - colName.Length); j++){
+                        output += " ";
+                    }
+                    output += "|";  
+                }
+
+                output += "\n";
+                for(int j=0; j<(colsAmount*colWidht)+colsAmount; j++){output += "-";}
+                output += "\n";
+
+                // add records
+                while(dataReader.Read()){
+                    output += ReadSingleRow((IDataRecord)dataReader,colsAmount,colWidht);
+                }
+
+                return output;
+
+            }catch(SqlException ex){
+                return HandleSqlException(ex);
+            }
+            catch(Exception ex){
+                return HandleOtherException(ex);
+            }
+             
+        }
+
 
         // INSERT,UPDATE,DROP,DELETE, etc.
         public void ExecuteQuery(string sqlQuery){
-            SqlCommand command = new SqlCommand(sqlQuery,this._connection);
-            int num = command.ExecuteNonQuery();
-            Console.WriteLine("Num of edited rows: "+num);
+            
+            try{
+                SqlCommand command = new SqlCommand(sqlQuery,this._connection);
+                int num = command.ExecuteNonQuery();
+                Console.WriteLine("Num of edited rows: "+num);
+
+            }catch(SqlException ex){
+                Console.WriteLine(HandleSqlException(ex));
+
+            }catch(Exception ex){    
+                Console.WriteLine(HandleOtherException(ex)); 
+            }
+        }
+
+        public string HandleSqlException(SqlException ex){
+            StringBuilder errorMessages = new StringBuilder();
+            for (int i = 0; i < ex.Errors.Count; i++)
+                {
+                    errorMessages.Append("\nHandled exception \n"+
+                        "Index #" + i + "\n" +
+                        "Message: " + ex.Errors[i].Message + "\n" +
+                        "LineNumber: " + ex.Errors[i].LineNumber + "\n" +
+                        "Source: " + ex.Errors[i].Source + "\n" +
+                        "Procedure: " + ex.Errors[i].Procedure + "\n");
+                }
+                return errorMessages.ToString();
+        }
+
+        public string HandleOtherException(Exception ex){
+            StringBuilder errorMessages = new StringBuilder();
+            errorMessages.Append("\nHandled exception \n"+
+                    "Message: "+ex.Message+"\n"+
+                    "Source: " +ex.Source+"\n");
+            return errorMessages.ToString();
         }
 
     }
