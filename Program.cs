a0f76d6225ec9612b873787ae1491f08fffd8550
Maciej Koch <maciej.k00h@gmail.com>
2021-01-19 14:45:57 +0100

    add CheckIfTableExists() and GetColumnsName() to MsSqlConnection

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Design_Patterns_project.Attributes;
+using Design_Patterns_project.Connection;
 using System.Collections.Generic;
+
+
 
 namespace Design_Patterns_project
 {
@@ -13,30 +16,27 @@ namespace Design_Patterns_project
             //remote -> "den1.mssql7.gear.host", "DPTest", "dptest", "Me3JyhRLOg-_"
             //local -> "DESKTOP-HVUO0CP", "TestDB"
 
-            /*MsSqlConnectionConfig remoteConfig = new MsSqlConnectionConfig("den1.mssql7.gear.host", "DPTest", "dptest", "Me3JyhRLOg-_");
-            MsSqlConnectionConfig localConfig = new MsSqlConnectionConfig("DESKTOP-HVUO0CP", "TestDB");
+            // MsSqlConnectionConfig remoteConfig = new MsSqlConnectionConfig("den1.mssql7.gear.host", "DPTest", "dptest", "Me3JyhRLOg-_");
+            // MsSqlConnectionConfig localConfig = new MsSqlConnectionConfig("DESKTOP-HVUO0CP", "TestDB");
 
-            MsSqlConnection connection = new MsSqlConnection(remoteConfig);
-            connection.ConnectAndOpen();
+            // MsSqlConnection connection = new MsSqlConnection(remoteConfig);
+            // connection.ConnectAndOpen();
 
-            string testSelectQuery = "SELECT * FROM dbo.Players;";
-            string testInsertQuery = "INSERT INTO dbo.Players (PlayerID,Name,Surname,Age,Nick) VALUES (4,'Gerard','Pique',33,'Lion');";
-            string testDeleteQuery = "DELETE FROM dbo.Players WHERE PlayerID = 4;";
+            // string testSelectQuery = "SELECT * FROM dbo.Players;";
+            // string testInsertQuery = "INSERT INTO dbo.Players (PlayerID,Name,Surname,Age,Nick) VALUES (4,'Gerard','Pique',33,'Lion');";
+            // string testDeleteQuery = "DELETE FROM dbo.Players WHERE PlayerID = 4;";
 
-            string output = connection.ExecuteSelectQuery(testSelectQuery);
-            Console.WriteLine(output);
+            // string output = "";
+            // connection.ExecuteQuery(testInsertQuery);
+            // output = connection.ExecuteSelectQuery(testSelectQuery);
+            // Console.WriteLine(output);
+            
 
-            connection.ExecuteQuery(testInsertQuery);
-            output = connection.ExecuteSelectQuery(testSelectQuery);
-            Console.WriteLine(output);
-            connection.Dispose();
+            // connection.ExecuteQuery(testDeleteQuery);
+            // output = connection.ExecuteSelectQuery(testSelectQuery);
+            // Console.WriteLine(output);
 
-            connection.ExecuteQuery(testDeleteQuery);
-            output = connection.ExecuteSelectQuery(testSelectQuery);
-            Console.WriteLine(output);
-
-
-            connection.Dispose();*/
+            // connection.Dispose();
 
 
             IceDragon iceDragon = new IceDragon(200, "Winterrrer", 10, 20, 20, 50);
