c3b6e639efbc353d117dbe31ccc4c7a6a52e7152
Szymon Auguścik <szyauguscik@gmail.com>
2021-01-23 00:50:50 +0100

    Implemented CreateTable for Inheritance. Provided several tests for Inheritance and Relationships

---
+++ Tests/RelationshipsTest/RelationshipsTest.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Design_Patterns_project.Attributes;
+using System.Collections.Generic;
+using Design_Patterns_project;
+
+namespace RelationshipsTest
+{
+    class RelationshipsTest
+    {
+        public static void Main(string[] args)
+        {
+            // remote -> "den1.mssql7.gear.host", "DPTest", "dptest", "Me3JyhRLOg-_"
+
+            // local 
+            // maciopelo -> "DESKTOP-HVUO0CP", "TestDB"
+            // szymon -> "LAPTOP-BHF7G1P9", "Test" databases (like tests directories)
+
+            DataManager mountainManager = new DataManager("LAPTOP-BHF7G1P9", "RelationshipsTest");
+
+            Dog testDog = new Dog(1, "Burek", 6);
+            Sheep sheep1 = new Sheep(1, "Marcysia", 2.35);
+            Sheep sheep2 = new Sheep(2, "Pola", 1.84);
+            Sheep sheep3 = new Sheep(3, "Hania", 1.27);
+            Alp alp1 = new Alp(1, "Rozlegla dolina", 5.61);
+            Alp alp2 = new Alp(2, "Gorska tajemnica", 7.42);
+            Shepherd testShepherd = new Shepherd(1, "Franek", testDog);
+
+            testShepherd.AddSheep(sheep1);
+            testShepherd.AddSheep(sheep2);
+            testShepherd.AddSheep(sheep3);
+
+            testShepherd.AddAlp(alp1);
+            testShepherd.AddAlp(alp2);
+
+            mountainManager.CreateTable(testShepherd);
+
+            Console.WriteLine("Utter success");
+        }
+    }
+
+    [Table("pasterz")]
+    class Shepherd
+    {
+        [PKey()]
+        [Column("identyfikator")]
+        int id { get; set; }
+
+        [Column("imie")]
+        string name { get; set; }
+
+        [OneToOne]
+        Dog dog { get; set; }
+
+        [OneToMany]
+        List<Sheep> sheep { get; set; } = new List<Sheep>();
+
+        [ManyToMany]
+        List<Alp> alps { get; set; } = new List<Alp>();
+
+        public Shepherd(int id, string name, Dog dog)
+        {
+            this.id = id;
+            this.name = name;
+            this.dog = dog;
+        }
+
+        public void AddSheep(Sheep newSheep)
+        {
+            this.sheep.Add(newSheep);
+        }
+
+        public void AddAlp(Alp newAlp)
+        {
+            this.alps.Add(newAlp);
+        }
+    }
+
+    [Table("pies")]
+    class Dog
+    {
+        [PKey()]
+        [Column("identyfikator")]
+        int id { get; set; }
+
+        [Column("imie")]
+        string name { get; set; }
+
+        [Column("wiek")]
+        double age { get; set; }
+
+        public Dog(int id, string name, double age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    [Table("owca")]
+    class Sheep
+    {
+        [PKey()]
+        [Column("identyfikator")]
+        int id { get; set; }
+
+        [Column("imie")]
+        string name { get; set; }
+
+        [Column("jakosc_welny")]
+        double woolQuality { get; set; }
+
+        public Sheep(int id, string name, double woolQuality)
+        {
+            this.id = id;
+            this.name = name;
+            this.woolQuality = woolQuality;
+        }
+    }
+
+    [Table("hala_gorska")]
+    class Alp
+    {
+        [PKey()]
+        [Column("identyfikator")]
+        int id { get; set; }
+
+        [Column("nazwa")]
+        string name { get; set; }
+
+        [Column("powierzchnia")]
+        double area { get; set; }
+
+        public Alp(int id, string name, double area)
+        {
+            this.id = id;
+            this.name = name;
+            this.area = area;
+        }
+    }
+}
---
+++ Tests/SingleTableInheritanceTest/SingleTableInheritanceTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Design_Patterns_project.Attributes;
+using System.Collections.Generic;
+using Design_Patterns_project;
+
+namespace SingleTableInheritanceTest
+{
+    class SingleTableInheritanceTest
+    {
+        public static void Main(string[] args)
+        {
+            // remote -> "den1.mssql7.gear.host", "DPTest", "dptest", "Me3JyhRLOg-_"
+
+            // local 
+            // maciopelo -> "DESKTOP-HVUO0CP", "TestDB"
+            // szymon -> "LAPTOP-BHF7G1P9", "Test" databases (like tests directories)
+
+            DataManager mythicalManager = new DataManager("LAPTOP-BHF7G1P9", "SingleTableInheritanceTest");
+
+            IceDragon iceDragon = new IceDragon(200, "Winter", 15, 20, 35, 50);
+            GoldDragon goldDragon = new GoldDragon(220, "Smaug", 10, 20, 40, 40);
+            List<Object> creatures = new List<Object>() { iceDragon, goldDragon };
+
+            mythicalManager.Inherit(creatures, 0);
+
+            Console.WriteLine("Utter success");
+        }
+    }
+
+    class MythicalCreature
+    {
+        [PKey()]
+        [Column()]
+        int id { get; set; }
+
+        [Column()]
+        int health { get; set; }
+
+        [Column("mythical_name")]
+        string name { get; set; }
+
+        public MythicalCreature(int health, string name)
+        {
+            this.health = health;
+            this.name = name;
+        }
+    }
+
+    class Dragon : MythicalCreature
+    {
+        [Column("blast_power")]
+        int blastPower { get; set; }
+
+        [Column()]
+        int endurance { get; set; }
+
+        public Dragon(int health, string name, int blastPower, int endurance) : base(health, name)
+        {
+            this.blastPower = blastPower;
+            this.endurance = endurance;
+        }
+    }
+
+    class GoldDragon : Dragon
+    {
+        [Column("mineral_hunger")]
+        int mineralHunger { get; set; }
+
+        [Column()]
+        double preciousness { get; set; }
+
+        public GoldDragon(int health, string name, int fireCapacity, int endurance, int mineralHunger, double preciousness)
+            : base(health, name, fireCapacity, endurance)
+        {
+            this.mineralHunger = mineralHunger;
+            this.preciousness = preciousness;
+        }
+    }
+
+    class IceDragon : Dragon
+    {
+        [Column("time_freeze")]
+        int timeFreeze { get; set; }
+
+        [Column("ice_fire")]
+        double iceFire { get; set; }
+
+        public IceDragon(int health, string name, int fireCapacity, int endurance, int timeFreeze, double iceFire)
+            : base(health, name, fireCapacity, endurance)
+        {
+            this.timeFreeze = timeFreeze;
+            this.iceFire = iceFire;
+        }
+    }
+}
