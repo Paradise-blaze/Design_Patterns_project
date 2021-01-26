438fda378b4942155f6f28845be895201cd7f63a
Szymon Auguścik <szyauguscik@gmail.com>
2021-01-26 17:32:10 +0100

    Delete ready

--- Tests/ClassTableInheritanceTest/ClassTableInheritanceTest.cs
+++ Tests/ClassTableInheritanceTest/ClassTableInheritanceTest.cs
@@ -17,16 +17,32 @@ namespace ClassTableInheritanceTest
 
             DataManager wizardManager = new DataManager("LAPTOP-BHF7G1P9", "ClassTableInheritanceTest");
 
-            ArchMage archMage = new ArchMage(1, "Gandalf", 1, 2.32, 1, 106);
-            DarkMage darkMage = new DarkMage(2, "Saruman", 1, 2.26, 1, 89);
-            Mage mage = new Mage(3, "Radagast", 1, 67);
-            Character character = new Character(4, "Bilbo");
-            List<Object> mages = new List<Object>() { archMage, darkMage };
+            ArchMage archMage1 = new ArchMage(1, "Gandalf", 1, 2.32, 1, 106);
+            ArchMage archMage2 = new ArchMage(1, "Vilgefortz", 1, 2.29, 2, 104);
+            DarkMage darkMage1 = new DarkMage(1, "Saruman", 1, 2.26, 1, 89);
+            DarkMage darkMage2 = new DarkMage(1, "Dark Wizard", 1, 1.73, 2, 70);
+            Mage mage1 = new Mage(1, "Radagast", 1, 67);
+            Mage mage2 = new Mage(1, "Merlin", 2, 86);
+            Character character1 = new Character(1, "Bilbo Baggins");
+            Character character2 = new Character(2, "Dijkstra");
+            Character character3 = new Character(3, "King Arthur");
+            List<Object> mages = new List<Object>() { archMage1, darkMage1 };
+
             wizardManager.Inherit(mages, 1);
-            wizardManager.Insert(archMage);
-            wizardManager.Insert(darkMage);
-            wizardManager.Insert(mage);
-            wizardManager.Insert(character);
+
+            wizardManager.Insert(archMage1);
+            wizardManager.Insert(archMage2);
+            wizardManager.Insert(darkMage1);
+            wizardManager.Insert(darkMage2);
+            wizardManager.Insert(mage1);
+            wizardManager.Insert(mage2);
+            wizardManager.Insert(character1);
+            wizardManager.Insert(character2);
+            wizardManager.Insert(character3);
+
+            wizardManager.Delete(archMage2);
+            List<SqlCondition> conditions = new List<SqlCondition> { SqlCondition.LowerThan("id", 2) };
+            wizardManager.Delete("darkMage", conditions);
 
             Console.WriteLine("Utter success");
         }
@@ -76,7 +92,7 @@ namespace ClassTableInheritanceTest
         public ArchMage(int id, string name, int mageID, double spell_skills, int archMageID, int yearsOfExperience)
             : base(id, name, mageID, spell_skills)
         {
-            this.archMageID = mageID;
+            this.archMageID = archMageID;
             this.yearsOfExperience = yearsOfExperience;
         }
     }
--- Tests/ConcreteTableInheritance/ConcreteTableInheritanceTest.cs
+++ Tests/ConcreteTableInheritance/ConcreteTableInheritanceTest.cs
@@ -17,16 +17,38 @@ namespace ConcreteTableInheritanceTest
 
             DataManager vehicleManager = new DataManager("LAPTOP-BHF7G1P9", "ConcreteTableInheritanceTest");
 
-            FourWheeledVehicle car = new FourWheeledVehicle(1, 250.21, 8.12);
-            TwoWheeledVehicle motorbike = new TwoWheeledVehicle(1, 166.67, 20.51);
-            Vehicle truck = new Vehicle(1, 105.51);
-            Vehicle bike = new Vehicle(2, 51.66);
-            List<Object> vehicles = new List<Object>() { car, motorbike };
+            FourWheeledVehicle car1 = new FourWheeledVehicle(1, 250.21, 8.12);
+            FourWheeledVehicle car2 = new FourWheeledVehicle(2, 245.15, 7.91);
+            FourWheeledVehicle car3 = new FourWheeledVehicle(3, 234.59, 8.65);
+            TwoWheeledVehicle motorbike1 = new TwoWheeledVehicle(1, 154.15, 18.91);
+            TwoWheeledVehicle motorbike2 = new TwoWheeledVehicle(2, 166.67, 20.51);
+            TwoWheeledVehicle motorbike3 = new TwoWheeledVehicle(3, 165.51, 21.51);
+            Vehicle truck1 = new Vehicle(1, 105.51);
+            Vehicle truck2 = new Vehicle(2, 99.81);
+            Vehicle truck3 = new Vehicle(3, 101.74);
+            Vehicle truck4 = new Vehicle(4, 106.12);
+            Vehicle truck5 = new Vehicle(5, 113.95);
+            Vehicle bike1 = new Vehicle(6, 51.66);
+            Vehicle bike2 = new Vehicle(7, 48.91);
+            List<Object> vehicles = new List<Object>() { car1, motorbike1 };
             vehicleManager.Inherit(vehicles, 2);
-            vehicleManager.Insert(car);
-            vehicleManager.Insert(motorbike);
-            vehicleManager.Insert(truck);
-            vehicleManager.Insert(bike);
+            vehicleManager.Insert(car1);
+            vehicleManager.Insert(car2);
+            vehicleManager.Insert(car3);
+            vehicleManager.Insert(motorbike1);
+            vehicleManager.Insert(motorbike2);
+            vehicleManager.Insert(motorbike3);
+            vehicleManager.Insert(truck1);
+            vehicleManager.Insert(truck2);
+            vehicleManager.Insert(truck3);
+            vehicleManager.Insert(truck4);
+            vehicleManager.Insert(truck5);
+            vehicleManager.Insert(bike1);
+            vehicleManager.Insert(bike2);
+
+            List<SqlCondition> conditions = new List<SqlCondition> { SqlCondition.GreaterThan("size", 8) };
+            vehicleManager.Delete("FourWheeledVehicle", conditions);
+            vehicleManager.Delete(motorbike2);
 
             Console.WriteLine("Utter success");
         }
